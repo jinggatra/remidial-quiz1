33fc9774e01d1a50a6e96a220b26c19c7a69f8c6
J I N G G A <J I N G G A@DESKTOP-FVRNFBC>
2018-11-04 17:35:56 +0700

    Pada class Karyawan, terdapat atribut nama, np, goldar, gaji, dan potongan juga terdapat 2 konstrukutor (berparameter dayang tanpa parameter) Method set dan get dan method untuk menghitung gaji

---
+++ src/com/taufik/remidi/Karyawan.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.taufik.remidi;
+
+/**
+ *
+ * @author J I N G G A
+ */
+public class Karyawan{
+    private String nama;
+    private String np;
+    private String goldar;
+    private double gaji;
+    private float potongan = 0.5f;
+
+    public Karyawan() {
+        
+    }
+    
+    
+    public Karyawan(String nama, String np, String goldar) {
+        this.nama = nama;
+        this.np = np;
+        this.goldar = goldar;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNp() {
+        return np;
+    }
+
+    public void setNp(String np) {
+        this.np = np;
+    }
+
+    public String getGoldar() {
+        return goldar;
+    }
+
+    public void setGoldar(String goldar) {
+        this.goldar = goldar;
+    }
+    
+    public double getGaji() {
+        return gaji;
+    }
+
+    public void setGaji(double gaji) {
+        this.gaji = gaji;
+    }
+    
+    public double hitungGaji(){
+        return gaji*potongan;
+    }
+       
+    public void info(){
+        System.err.println("Nama \t\t: "+nama);
+        System.err.println("ID Karyawan \t: "+np);
+        System.err.println("Golongan Darah \t: "+ goldar);
+
+    }
+}
